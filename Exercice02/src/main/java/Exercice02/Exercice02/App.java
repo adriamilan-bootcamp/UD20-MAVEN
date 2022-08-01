9a5dd6898a5bb0eed2aad7a63b7ee3bc412f0535
Adrià Milan <adria4milan@gmail.com>
2022-08-01 10:54:36 +0200

    Exercice02 v1.1

---
+++ Exercice02/src/main/java/Exercice02/Exercice02/App.java
@@ -0,0 +1,10 @@
+package Exercice02.Exercice02;
+import Exercice02.Interaccion.AplicacionGrafica;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        AplicacionGrafica window = new AplicacionGrafica();
+    }
+}
---
+++ Exercice02/src/main/java/Exercice02/Interaccion/AplicacionGrafica.java
@@ -0,0 +1,65 @@
+package Exercice02.Interaccion;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class AplicacionGrafica extends JFrame {
+
+	private JPanel contentPane;
+	
+	public AplicacionGrafica() {
+		
+		// Anadimos un titulo al programa
+		setTitle("Exercice 01");
+		
+		// Coordenadas x y  de la aplicacion
+		setBounds(400, 200, 275, 132);
+		
+		// Indica cuando se cierra la ventana se cierra la aplicacion
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		// Hace visible la ventana
+		setVisible(true);
+		
+		// Creamos el panel
+		contentPane = new JPanel();
+		
+		// Indicamos el diseno del panel
+		contentPane.setLayout(null);
+		
+		// Asignamos el panel a la ventana
+		setContentPane(contentPane);
+		
+		// Mensaje inicial
+		final String message = "Has pulsado el boton:";
+		// Label inicial
+		JLabel textInfoBtn = new JLabel(message);
+		textInfoBtn.setBounds(12, 12, 297, 15);
+		contentPane.add(textInfoBtn);
+		
+		// Boton numero 1
+		JButton boton1 = new JButton("Boton1");
+		boton1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Label inicial + nombre del boton pulsado
+				textInfoBtn.setText(message + " Boton1");
+			}
+		});
+		boton1.setBounds(12, 39, 117, 25);
+		contentPane.add(boton1);
+		
+		// Boton numero 2
+		JButton boton2 = new JButton("Boton2");
+		boton2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Label inicial + nombre del boton pulsado
+				textInfoBtn.setText(message + " Boton2");
+			}
+		});
+		boton2.setBounds(141, 39, 117, 25);
+		contentPane.add(boton2);
+		
+	}
+	
+}
