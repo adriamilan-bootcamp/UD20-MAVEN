bd1327e6baab067ea1af06943e321ab41064e1e5
Adrià Milan <adria4milan@gmail.com>
2022-08-01 09:24:42 +0200

    Test Maven

---
+++ Exercice01/src/main/java/Exercice01/Exercice01/App.java
@@ -0,0 +1,13 @@
+package Exercice01.Exercice01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
